b144604970d405f8f44c61944838a5b160f267da
j0s3-s4nch3z <frogapp2019@gmail.com>
2019-01-28 23:45:14 -0300

    first commit

---
+++ main.ino
@@ -0,0 +1,14 @@
+void setup()  {
+  Serial.begin(9600);
+  pinMode(6,INPUT);
+  digitalWrite(6,LOW);
+}
+void loop()  {
+    if(digitalRead(6)==HIGH)  {
+      Serial.println("Somebody is here.");
+    }
+    else  {
+      Serial.println("Nobody.");
+    }
+    delay(1000);
+}
